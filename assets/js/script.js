daebb3d804846f675af2567dc879971ff07e8b65
Brian Choi <brianchoix31@gmail.com>
2022-07-25 15:22:59 -0400

    created input variables for each of user's selection + 'if' statement for length

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,37 @@ var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // created generate password function
 function generatePassword() {
+// created input variable for user's character length selection
+  var lengthInput = window.prompt("How many characters would you like your password to contain? (8-128 characters)");
+// created 'if' statement with 'or' operator so that user must select integer between 8-128, otherwise error alert appears, then returns the user back to start
+    if (lengthInput < 8 || lengthInput > 128) {
+      window.alert("Select a value between 8-128.");
+      return;
+    }
+// created input variable for user's special option selection
+  var specialCharInput = confirm("Click OK to confirm including special characters.");
 
-  prompt("How many characters would you like your password to contain? (8-128 characters)")
+    if (specialCharInput) {
+
+    }
+// created input variable for user's numeric option selection  
+  var numericCharInput = confirm("Click OK to confirm including numeric characters.");
+
+    if (numericCharInput) {
+        
+    }
+// created input variable for user's lowercase option selection
+  var lowercaseCharInput = confirm("Click OK to confirm including lowercase characters.");
+
+    if (lowercaseCharInput) {
+          
+    }
+// created input variable for user's uppercase option selection
+  var uppercaseCharInput = confirm("Click OK to confirm including uppercase characters.");
+
+    if (uppercaseCharInput) {
+            
+    }
 
   return password;
 }
