f1ade5769bfbd841a98db665a2200c6cdf7f8891
Brian Choi <brianchoix31@gmail.com>
2022-07-25 23:12:09 -0400

    simplified/rearranged variables + cleaned up addition operator statements + reworked math floor / math random to function properly in generated password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,64 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-// created character variables
-var specialChar = "~!@#$%^&*()_+";
-var numericChar = "0123456789";
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
-// created generate password function
-function generatePassword() {
-// created input variable for user's character length selection
-  var lengthInput = window.prompt("How many characters would you like your password to contain? (8-128 characters)");
-// created 'if' statement with 'or' operator so that user must select integer between 8-128, otherwise error alert appears, then returns the user back to start
-    if (lengthInput < 8 || lengthInput > 128) {
-      window.alert("Select a value between 8-128.");
-      return;
-    }
-// created input variable for user's special option selection
-  var specialCharInput = confirm("Click OK to confirm including special characters.");
-
-    if (specialCharInput) {
-
-    }
-// created input variable for user's numeric option selection  
-  var numericCharInput = confirm("Click OK to confirm including numeric characters.");
-
-    if (numericCharInput) {
-        
-    }
-// created input variable for user's lowercase option selection
-  var lowercaseCharInput = confirm("Click OK to confirm including lowercase characters.");
-
-    if (lowercaseCharInput) {
-          
-    }
-// created input variable for user's uppercase option selection
-  var uppercaseCharInput = confirm("Click OK to confirm including uppercase characters.");
-
-    if (uppercaseCharInput) {
-            
-    }
-
-  return password;
-}
-
-
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// created character variables
+var special = "~!@#$%^&*()_+";
+var numeric = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// created generate password function
+function generatePassword() {
+
+  // generated password variable
+  var generatedPassword = "";
+  // user selected variable
+  var selected = "";
+
+  // created password length variable
+  var length = window.prompt("How many characters would you like your password to contain? (8-128 characters)");
+    // created 'if' statement with 'or' operator so user must select integer between 8-128, otherwise error alert appears, then returns the user back to start
+    if (length < 8 || length > 128) {
+      window.alert("Select a value between 8-128.");
+      return;
+    }
+
+  // user input variables for each character selected by the user along with 'if' statement with 'addition' operator to be/not to be included with user's selections
+  var specialInput = window.confirm("Click OK to confirm including special characters.");
+    if (specialInput) {
+      selected += special
+    }
+
+  var numericInput = window.confirm("Click OK to confirm including numeric characters.");
+    if (numericInput) {
+      selected += numeric
+    }
+
+  var lowercaseInput = window.confirm("Click OK to confirm including lowercase characters.");
+    if (lowercaseInput) {
+      selected += lowercase
+    }
+
+  var uppercaseInput = window.confirm("Click OK to confirm including uppercase characters.");
+    if (uppercaseInput) {
+      selected += uppercase
+    }
+
+var password = "";
+
+// math floor & math random commands randomizes all of user's selections
+for (i = 0; i < length; i++) {
+    generatedPassword = [Math.floor(Math.random() * selected.length)];
+    password = password + selected[generatedPassword];
+}
+
+return password;
+
+}
